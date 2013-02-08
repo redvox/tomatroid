94e812039d7841711903afe45b02259059acd075
redvox <redpaperfox@gmail.com>
2013-02-08 17:44:40 +0100

    Found a way to add bars to a linear layout, the space is automatically equally shared by the bars.

--- src/com/example/tomatroid/MainActivity.java
+++ src/com/example/tomatroid/MainActivity.java
@@ -1,20 +1,61 @@
 package com.example.tomatroid;
 
+import com.example.tomatroid.digram.Bar;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
 
 public class MainActivity extends Activity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	LayoutParams params = new LayoutParams(LayoutParams.MATCH_PARENT,
+			LayoutParams.MATCH_PARENT);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
+	LayoutParams barParams = new TableLayout.LayoutParams(
+			LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, 1f);
+
+	LinearLayout digram;
+
+	View bar1;
+	View bar2;
+	View bar3;
+	View bar4;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		digram = (LinearLayout) findViewById(R.id.digram);
+		
+		bar1 = new Bar(digram.getContext(), 60);
+//		bar1.setPadding(10, 0, 10 ,0);
+		bar2 = new Bar(digram.getContext(), 30);
+//		bar2.setPadding(10, 0, 10 ,0);
+		bar3 = new Bar(digram.getContext(), 15);
+//		bar3.setPadding(10, 0, 10 ,0);
+		bar4 = new Bar(digram.getContext(), 55);
+		
+		digram.addView(bar1, barParams);
+		digram.addView(bar2, barParams);
+		digram.addView(bar3, barParams);
+		digram.addView(bar4, barParams);
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
 }
---
+++ src/com/example/tomatroid/digram/Bar.java
@@ -0,0 +1,72 @@
+package com.example.tomatroid.digram;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.util.Log;
+import android.view.View;
+
+public class Bar extends View {
+
+	Context context;
+	int value = 0;
+//	float maxW = 0;
+//	float maxH = 0;
+	float width = 0;
+	float height = 0;
+
+	int minMax = 60;
+	float pointPerMinute = 0;
+	
+	Paint color;
+	RectF bar;
+
+	public Bar(Context context, int value) {
+		super(context);
+		this.context = context;
+		this.value = value;
+		
+		color = new Paint();
+		color.setColor(Color.GRAY);
+		color.setShadowLayer(2f, 1f, 1f, Color.BLACK);
+		
+		bar = new RectF();
+	}
+	
+	public void setValue(int newValue){
+		
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas) {
+		super.onDraw(canvas);
+		canvas.drawRect(0, height - (value*pointPerMinute), width, height, color);
+		
+	}
+
+	@Override
+	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+		super.onSizeChanged(w, h, oldw, oldh);
+
+		// Account for padding
+		float xpad = (float) (getPaddingLeft() + getPaddingRight());
+		float ypad = (float) (getPaddingTop() + getPaddingBottom());
+
+		// Account for the label
+//		if (mShowText)
+//			xpad += mTextWidth;
+
+		width = (float) w - xpad;
+		height = (float) h - ypad;
+		
+		pointPerMinute = height / minMax;
+
+//		Log.e("Bar", "ppM " + pointPerMinute);
+//		Log.e("Bar", "h" + height);
+		
+		// Figure out how big we can make the pie.
+//		float diameter = Math.min(ww, hh);
+	}
+}
