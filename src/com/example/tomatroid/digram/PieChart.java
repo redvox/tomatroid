35ea6394fd1cd8c9a08d033f42ebac4ac18c25e7
redvox <redpaperfox@gmail.com>
2013-04-04 10:08:24 +0200

    Refine PieChart implementation.

--- src/com/example/tomatroid/digram/PieChart.java
+++ src/com/example/tomatroid/digram/PieChart.java
@@ -6,6 +6,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
 import android.graphics.Paint.Style;
+import android.util.AttributeSet;
 import android.view.View;
 
 public class PieChart extends View {
@@ -13,30 +14,29 @@ public class PieChart extends View {
 	private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
 	private Paint paintBlackStroke = new Paint(Paint.ANTI_ALIAS_FLAG);
 	private float[] value_degree;
-	private int[] COLORS = { Color.BLUE, Color.GREEN, Color.GRAY, Color.CYAN,
+	private int[] colors = { Color.BLUE, Color.GREEN, Color.GRAY, Color.CYAN,
 			Color.RED, Color.YELLOW };
 
-	RectF rectf = new RectF(10, 10, 200, 200);
 	float width = 0;
 	float height = 0;
-	
-	
+	float offset = 5;
+	RectF rectf = new RectF(offset, offset, 200, 200);
+
 	public PieChart(Context context, float[] values) {
 		super(context);
-
 		paintBlackStroke.setStyle(Style.STROKE);
 		paintBlackStroke.setColor(Color.BLACK);
-		paintBlackStroke.setStrokeWidth(5);
+		paintBlackStroke.setStrokeWidth(2);
 
-		float total = 0;
-		value_degree = new float[values.length];
-		for (int i = 0; i < values.length; i++) {
-			total += values[i];
-		}
-		
-		for (int i = 0; i < values.length; i++) {
-			value_degree[i] = 360*(values[i]/total);
-		}
+		setValues(values);
+	}
+
+	// inflater constructor
+	public PieChart(Context context, AttributeSet attributeSet) {
+		super(context, attributeSet);
+		paintBlackStroke.setStyle(Style.STROKE);
+		paintBlackStroke.setColor(Color.BLACK);
+		paintBlackStroke.setStrokeWidth(2);
 	}
 
 	@Override
@@ -47,30 +47,50 @@ public class PieChart extends View {
 		for (int i = 0; i < value_degree.length; i++) {// values2.length; i++) {
 			if (i != 0)
 				temp += (int) value_degree[i - 1];
-			paint.setColor(COLORS[i]);
+			paint.setColor(colors[i]);
 			canvas.drawArc(rectf, temp, value_degree[i], true, paint);
 			canvas.drawArc(rectf, temp, value_degree[i], true, paintBlackStroke);
 		}
 	}
-	
+
 	@Override
 	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
 		super.onSizeChanged(w, h, oldw, oldh);
 
+		w = (int) (w-offset);
+		h = (int) (h-offset);
+		
 		// Account for padding
 		float xpad = (float) (getPaddingLeft() + getPaddingRight());
 		float ypad = (float) (getPaddingTop() + getPaddingBottom());
 
 		width = (float) w - xpad;
 		height = (float) h - ypad;
-		
+
+		float toCenterOffset = Math.abs(width - height)/2;
 		if(width > height){
 			width = height;
+			rectf = new RectF(offset+toCenterOffset, offset, width+toCenterOffset, height);
 		} else {
 			height = width;
-		}
-
-		rectf = new RectF(0, 0, width, height);
+			rectf = new RectF(offset, offset+toCenterOffset, width, height+toCenterOffset);
+		}	
 	}
 
+	public void setValues(float[] values) {
+		float total = 0;
+		value_degree = new float[values.length];
+		for (int i = 0; i < values.length; i++) {
+			total += values[i];
+		}
+
+		for (int i = 0; i < values.length; i++) {
+			value_degree[i] = 360 * (values[i] / total);
+		}
+	}
+
+	public void setValuesWithColor(float[] values, int[] colors) {
+		setValues(values);
+		this.colors = colors;
+	}
 }
